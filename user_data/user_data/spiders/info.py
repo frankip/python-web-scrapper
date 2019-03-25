5e3e371a2a690b762c8c864ba62ffe6c964ec82e
strongestavenger <mikemutoro@gmail.com>
2019-03-25 19:08:40 +0300

    add crawl functionality

--- user_data/user_data/spiders/info.py
+++ user_data/user_data/spiders/info.py
@@ -4,13 +4,28 @@ from scrapy.linkextractors import LinkExtractor
 
 class InfoSpider(CrawlSpider):
     name = 'info'
-    allowed_domains = ['https://www.jumia.co.ke']
-    start_urls = ['https://www.jumia.co.ke/electronics/',
-                  'https://www.jumia.co.ke/phones-tablets/'
+    allowed_domains = ['www.jumia.co.ke']
+    start_urls = ['https://www.jumia.co.ke/phones-tablets/',
+                    'https://www.jumia.co.ke/electronics/',
+                    'https://www.jumia.co.ke/grocery/',
+                    'https://www.jumia.co.ke/fashion/',
+                    'https://www.jumia.co.ke/automobile/',
+                    'https://www.jumia.co.ke/video-games/',
+                    'https://www.jumia.co.ke/home-office/',
+                    'https://www.jumia.co.ke/computing/',
+                    'https://www.jumia.co.ke/baby-products/',
+                    'https://www.jumia.co.ke/health-beauty/',
+
                     ]
     rules = (
         Rule(LinkExtractor(allow=(), restrict_css=('.item',)),
              callback="parse_item",
-             follow=False),)
+             follow=True),)
     def parse_item(self, response):
-        print('Processing..' + response.url)
+        import pdb;
+        # pdb.set_trace()
+        print('Processing.....' + response.url)
+        print("....")
+        print("...")
+        print("..")
+        print(".")
